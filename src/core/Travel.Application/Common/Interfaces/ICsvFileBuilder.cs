eaf6db2ba9537a26c2a27364c0facb098e84d433
emekat <emekatorti@gmail.com>
2023-03-12 15:30:44 -0400

    Added common interfaces

---
+++ src/core/Travel.Application/Common/Interfaces/ICsvFileBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Travel.Application.TourLists.QueriesAndHandlers.ExportTours;
+
+namespace Travel.Application.Common.Interfaces
+{
+    public interface ICsvFileBuilder
+    {
+        byte[] BuildTourPackagesFile(IEnumerable<TourPackageRecord> records); 
+    }
+}
