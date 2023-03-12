3034e88f34379218c276b32b28447c73b81719a5
emekat <emekatorti@gmail.com>
2023-03-12 15:31:25 -0400

    Added queries

---
+++ src/core/Travel.Application/TourLists/QueriesAndHandlers/ExportTours/ExportToursQuery.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Travel.Application.TourLists.QueriesAndHandlers.ExportTours
+{
+    public class ExportToursQuery: IRequest<ExportToursVm>
+    {
+        public int ListId { get; set; }
+    }
+
+    public class ExportToursQueryHandler : IRequestHandler<ExportToursQuery, ExportToursVm>
+    {
+        public async Task<ExportToursVm> Handle(ExportToursQuery request, CancellationToken
+            cancellationToken)
+        {
+            var vm = new ExportToursVm();
+            vm.ContentType = "text/csv";
+            vm.FileName = "TourPackages.csv";
+            return await Task.FromResult(vm);
+        }
+    }
+}
---
+++ src/core/Travel.Application/TourLists/QueriesAndHandlers/ExportTours/ExportToursVm.cs
@@ -0,0 +1,9 @@
+﻿namespace Travel.Application.TourLists.QueriesAndHandlers.ExportTours
+{
+    public class ExportToursVm
+    {
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
