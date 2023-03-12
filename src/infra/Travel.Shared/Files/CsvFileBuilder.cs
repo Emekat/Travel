67dea66b38741538292ab60a5d2c65e2f878174f
emekat <emekatorti@gmail.com>
2023-03-12 16:55:59 -0400

    Implemented Common interfaces in Shared lib

---
+++ src/infra/Travel.Shared/Files/CsvFileBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Travel.Application.Common.Interfaces;
+using Travel.Application.TourLists.QueriesAndHandlers.ExportTours;
+
+namespace Travel.Shared.Files
+{
+    public class CsvFileBuilder : ICsvFileBuilder
+    {
+        public byte[] BuildTourPackagesFile(IEnumerable<TourPackageRecord> records)
+        {
+            using var memoryStream = new MemoryStream();
+            return memoryStream.ToArray();
+        }
+    }
+}
